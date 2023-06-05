b7f00dcb05c6746f2093ddd8601f856fa1eb63ab
PRODIIF <125951820+michelstoc@users.noreply.github.com>
2023-06-05 16:25:01 +0200

    ok

--- app.py
+++ app.py
@@ -17,41 +17,46 @@ def main():
     st.set_page_config(page_title="PDF Query")
     st.header("Posez des questions sur le contenu d'un PDF")
     
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    # Ask for usage code
+    usage_code = st.text_input("Entrez votre code d'utilisation:")
     
-    # extract the text
-    if pdf is not None:
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
-        text += page.extract_text()
+    if usage_code == 'xyz':
+    
+        # upload file
+        pdf = st.file_uploader("Chargez votre PDF", type="pdf")
         
-      # split into chunks
-      text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-      )
-      chunks = text_splitter.split_text(text)
-      
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
-      
-      # show user input
-      user_question = st.text_input("Posez votre question:")
-      if user_question:
-        docs = knowledge_base.similarity_search(user_question)
+        # extract the text
+        if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+            
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
         
-        llm = OpenAI()
-        chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
-           
-        st.write(response)
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        
+        # show user input
+        user_question = st.text_input("Posez votre question:")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+            
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+            response = chain.run(input_documents=docs, question=user_question)
+            print(cb)
+            
+            st.write(response)
     
 
 if __name__ == '__main__':
